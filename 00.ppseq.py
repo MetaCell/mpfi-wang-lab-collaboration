390de57bd9ba56b2a1870ec65fbef31b36bebc07
phildong <35715936+phildong@users.noreply.github.com>
2024-12-17 18:25:14 +0000

    update parameters

--- 00.ppseq.py
+++ 00.ppseq.py
@@ -1,13 +1,16 @@
 # %% import and definition
 import os
+import pickle as pkl
 
 import numpy as np
 import plotly.express as px
 import torch
 import xarray as xr
 from ppseq.model import PPSeq
+from scipy.signal import medfilt
+from scipy.stats import zscore
 
-from routine.io import load_spks
+from routine.io import load_F, load_spks
 from routine.plotting import ppseq_plot_scatter, ppseq_plot_temp
 from routine.ppseq import thres_int
 
@@ -44,9 +47,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 spk_dat = torch.from_numpy(spk.values)
 torch.manual_seed(0)
 model = PPSeq(
-    num_templates=1,
+    num_templates=2,
     num_neurons=int(spk.sizes["cell"]),
-    template_duration=150,
+    template_duration=100,
     alpha_a0=1.5,
     beta_a0=0.2,
     alpha_b0=1,
@@ -55,9 +58,29 @@ model = PPSeq(
     beta_t0=0.1,
 )
 lps, amplitudes = model.fit(spk_dat, num_iter=100)
+with open(os.path.join(INT_PATH, "model.pkl"), "wb") as pklf:
+    pkl.dump(
+        {"model": model, "X": spk_dat.cpu(), "lps": lps, "amp": amplitudes.cpu()}, pklf
+    )
 
 # %% plotting
-fig_temp = ppseq_plot_temp(spk_dat.cpu(), model, amplitudes.cpu())
+with open(os.path.join(INT_PATH, "model.pkl"), "rb") as pklf:
+    ds = pkl.load(pklf)
+model, amp, X = ds["model"], ds["amp"], ds["X"]
+F = load_F(IN_DPATH)
+F = (
+    xr.apply_ufunc(
+        lambda x: zscore(medfilt(x, kernel_size=31)),
+        F,
+        input_core_dims=[["frame"]],
+        output_core_dims=[["frame"]],
+        vectorize=True,
+    )
+    .coarsen({"frame": PARAM_DS}, boundary="trim")
+    .mean()
+    .clip(0, 5)
+)
+fig_temp = ppseq_plot_temp(X, model, amp)
 fig_temp.write_html(os.path.join(FIG_PATH, "temps.html"))
-fig_scatter = ppseq_plot_scatter(spk_dat.cpu(), model, amplitudes.cpu())
+fig_scatter = ppseq_plot_scatter(X, model, amp, raw_dat=F)
 fig_scatter.write_html(os.path.join(FIG_PATH, "scatter.html"))
